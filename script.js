ed7e75e0dd85b07fe5e705d2229ca9939c845fc0
Paterne <angepaterne.dali@gmail.com>
2024-10-29 00:59:23 +0100

    first commit

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const dbPort = process.env.DB_PORT
   
 //BDD
 
-const pool = mariadb.createPool({
+const connexion = mariadb.createConnection({
     host:dbHost,
     database: dbName,
     user:dbUser, 
@@ -36,98 +36,132 @@ const pool = mariadb.createPool({
 
 //PRODUCTS
 
-app.get("/products",async (req,res) => {
-  const connexion = await pool.getConnection()
-  const allProducts = await connexion.query("SELECT * FROM products");
-  res.status(200).json(allProducts)
+app.get( "/products" , async (req,res) => {
+  try{
+    conn = await connexion;
+    const allProducts = await conn.query( "SELECT * FROM products" );
+    res.status( 200 ).json( allProducts )
+  }catch( err ){
+      console.log( err )
+      res.status( 404 ).json( { message: "Something wrong hapened"  } )
+  }
 })
 
 
-app.get("/products/:id",async (req,res) => {
-    const id = req.params.id
-    const connexion = await pool.getConnection()
-    const oneProduct = await connexion.query("SELECT * FROM products WHERE productId="+id);
-    res.status(200).json(oneProduct)
-})
-
-
-//ClIENTS
-
-app.get("/clients",async (req,res) => {
-  const id = req.params.id
-  const connexion = await pool.getConnection()
-  const oneProduct = await connexion.query("SELECT * FROM clients");
-  res.status(200).json(oneProduct)
-})
-
-app.get("/clients/:id",async (req,res) => {
-    const id = req.params.id
-    const connexion = await pool.getConnection()
-    const oneProduct = await connexion.query("SELECT * FROM clients WHERE clientId="+id);
-    res.status(200).json(oneProduct)
-})
-
-// test 
-app.put('/client/:id',async (req,res)=>{
+app.get( "/products/:id" , async (req,res) => {
     try{
-      const id = req.params.id
-      const { firstname,lastName, email, password } = req.body
-      sqlQuery = "UPDATE clients SET firstName =?, lastName = ?, email=?, password =?"
-      const connexion = await pool.getConnection()
-      connexion.query(sqlQuery,[ firstname,lastName,email,password ])
-    }catch(err){
+        const id = req.params.id
+        const conn = await connexion
+        const oneProduct = await conn.query( "SELECT * FROM products WHERE productId=" +id );
+        res.status(200).json(oneProduct)
+    }catch( err ){
         console.log(err)
+        res.status( 404 ).json( { message: "Something wrong hapened"  } )
     }
 })
 
-//ORDERS
 
-app.get("/orders",async (req,res) => {
+app.put( "/products/:id" , async (req,res) => {
+    try{
+        const id = req.params.id
+        const newValues = req.body
+        const conn  = await connexion
+        const keys = Object.keys( newValues )
+        
+        //logique à continuer
+
+        const oneProduct = await conn.query( "UPDATE products SET ?=?" +id );
+        res.status( 200 ).json( oneProduct )
+    }catch(err){
+        console.log(err)
+        res.status( 404 ).json( { message: "Something wrong hapened"  } )
+    }
+})
+
+
+app.delete("/products/:id",async (req,res) => {
   const id = req.params.id
   const connexion = await pool.getConnection()
-  const orders = await connexion.query("SELECT * FROM clients");
-  res.status(200).json(orders)
+  const oneProduct = await connexion.query("SELECT * FROM products WHERE productId="+id);
+  res.status(200).json(oneProduct)
 })
 
-app.get("/orders/:id",async (req,res) => {
-    const id = req.params.id
-    const connexion = await pool.getConnection()
-    const oneOrder = await connexion.query("SELECT * FROM orders WHERE orderID="+id);
-    res.status(200).json(oneOrder)
-})
 
-// test 
-app.put('/order/:id',async (req,res)=>{
-  try{
-    const id = req.params.id
-    const { firstname,lastName, email, password } = req.body
-    sqlQuery = "UPDATE clients SET firstName =?, lastName = ?, email=?, password =?"
-    const connexion = await pool.getConnection()
-    connexion.query(sqlQuery,[ firstname,lastName,email,password ])
-  }catch(err){
-      console.log(err)
-  }
-})
+// //ClIENTS
 
-//Postman test
+// app.get("/clients",async (req,res) => {
+//   const id = req.params.id
+//   const connexion = await pool.getConnection()
+//   const oneProduct = await connexion.query("SELECT * FROM clients");
+//   res.status(200).json(oneProduct)
+// })
 
-app.post( '/postman' ,(req,res)=>{
-  console.log( "request : "+ req.body )
-  try{
-      const { theme, question, response } = req.body
-      sqlStatement = "insert into requests( theme, question, response) values (?,?,?,?)"
-      const connexion = pool.getConnection()
-      connexion.query( sqlStatement,[ theme, question, response ] )
-      res.status( 200 ).send( { message: "operation successed" } )
-  }catch(err){
-      console.log(err)
-      res.status(200).send({message: "operation not successed"})
-  }
-})
+// app.get("/clients/:id",async (req,res) => {
+//     const id = req.params.id
+//     const connexion = await pool.getConnection()
+//     const oneProduct = await connexion.query("SELECT * FROM clients WHERE clientId="+id);
+//     res.status(200).json(oneProduct)
+// })
 
-app.listen(port,()=>{
-  console.log("server is connected")
-})
+// // test 
+// app.put('/client/:id',async (req,res)=>{
+//     try{
+//       const id = req.params.id
+//       const { firstname,lastName, email, password } = req.body
+//       sqlQuery = "UPDATE clients SET firstName =?, lastName = ?, email=?, password =?"
+//       const connexion = await pool.getConnection()
+//       connexion.query(sqlQuery,[ firstname,lastName,email,password ])
+//     }catch(err){
+//         console.log(err)
+//     }
+// })
+
+// //ORDERS
+
+// app.get("/orders",async (req,res) => {
+//   const id = req.params.id
+//   const connexion = await pool.getConnection()
+//   const orders = await connexion.query("SELECT * FROM clients");
+//   res.status(200).json(orders)
+// })
+
+// app.get("/orders/:id",async (req,res) => {
+//     const id = req.params.id
+//     const connexion = await pool.getConnection()
+//     const oneOrder = await connexion.query("SELECT * FROM orders WHERE orderID="+id);
+//     res.status(200).json(oneOrder)
+// })
+
+// // test 
+// app.put('/order/:id',async (req,res)=>{
+//   try{
+//     const id = req.params.id
+//     const { firstname,lastName, email, password } = req.body
+//     sqlQuery = "UPDATE clients SET firstName =?, lastName = ?, email=?, password =?"
+//     const connexion = await pool.getConnection()
+//     connexion.query(sqlQuery,[ firstname,lastName,email,password ])
+//   }catch(err){
+//       console.log(err)
+//   }
+// })
+
+// //Postman test
+
+// app.post( '/postman' ,(req,res)=>{
+//   console.log( "request : "+ req.body )
+//   try{
+//       const { theme, question, response } = req.body
+//       sqlStatement = "insert into requests( theme, question, response) values (?,?,?,?)"
+//       const connexion = pool.getConnection()
+//       connexion.query( sqlStatement,[ theme, question, response ] )
+//       res.status( 200 ).send( { message: "operation successed" } )
+//   }catch(err){
+//       console.log(err)
+//       res.status(200).send({message: "operation not successed"})
+//   }
+// })
+
+
 
 //LISTENER
 
