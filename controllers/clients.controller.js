1204339e0e781c736986757c74cae478420ce23c
Paterne <angepaterne.dali@gmail.com>
2024-10-29 23:28:41 +0100

    database.js rename to database.acess.js

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -1,4 +1,4 @@
-const db = require("../database/database")
+const db = require("../database/database.acces")
 const JSONbig = require('json-bigint')
 const bcrypt = require('bcrypt');
 
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,4 +1,4 @@
-const db = require("../database/database")
+const db = require("../database/database.acces")
 const JSONbig = require('json-bigint')
 
 exports.getAllProducts = async (req,res) => {
