ef6fa5e396aabb234887b4c8179af30708d75b66
Paterne <angepaterne.dali@gmail.com>
2024-10-30 02:06:26 +0100

    webtoken.js and morgan has been added

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -1,13 +1,17 @@
 const db = require("../database/database.acces")
 const JSONbig = require('json-bigint')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { token } = require("morgan");
+const ENV = require("../config");
+const { use } = require("../routes/products.routes");
 
 
 
 exports.getAllClients = async (req,res) => {
     try{
         const conn = await db.connexion
-        const oneProduct = await conn.query("SELECT * FROM clients");
+        const oneProduct = await conn.query("SELECT clientId, lastName, firstName, address FROM clients");
         res.status(200).json(oneProduct)
     }catch( error ){
         console.log(error)
@@ -22,7 +26,7 @@ exports.getSingleClient = async (req,res) => {
     try{
       const id = req.params.id
       const conn = await db.connexion
-      const allClients = await conn.query("SELECT * FROM clients WHERE clientId= ?",[id]);
+      const allClients = await conn.query("SELECT clientId, lastName, firstName, address FROM clients WHERE clientId= ?",[id]);
       res.status(200).json(allClients)
     }catch( error ){
         console.log(error)
@@ -107,12 +111,44 @@ exports.deleteSingleClient = async (req, res) => {
     }
     catch(error){
       console.log(error)
-      res.status(400).json({ message:"[CLIENT_DELETE] something went wrong: " + error })
+      res.status(400).json({ message:"[CLIENT_DELETE] something went wrong"})
     }
 }
 
 exports.openUserLogin = async (req, res) => {
 
+  try{
+    const id = req.params
+    const { address, password } = req.body
+    
+    const conn = await db.connexion
+    const user = await conn.query(`SELECT clientId, firstName, password FROM clients WHERE address='${address} '`)
+    if (user.length === 1) {
+
+       const passwordCheck = await bcrypt.compare(password, user[0].password)
+
+       if(!passwordCheck) return res.status(200).json({ message: "User don't have password"})
+
+        const token = jwt.sign({
+            userId: user.clientId,
+            username: user.firstName
+          }, ENV.JWT_SECERT,{expiresIn: "24h"})
+
+
+        return res.status(200).json({
+          message: "Login successfully...!",
+          username: user[0].firstName,
+          token
+        })  
+    }
+
+    res.status(200).json({ message: "User doesn't exist, change the field values and retry"})
+  }
+  catch(error){
+    console.log(error)
+    res.status(400).json({ message:"[LOGIN_SESSION] something went wrong"})
+  }
+  
 }
 
 exports.closeUserLogin = async (req, res) =>{
